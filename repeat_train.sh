0a5e2fa676e971d81ca92d8daf776f5d96bb865c
Jonah Krop <jonah.krop@ip-192-168-0-251.us-west-1.compute.internal>
2024-11-21 15:37:36 -0800

    updating bash script

--- repeat_train.sh
+++ repeat_train.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-#python3 transfer_learning.py --model='google/efficientnet-b3' --pretrained=False --train_test_split=0.25 --learning_rate=1e-3 --weight_decay=0
-#python3 transfer_learning.py --model='google/efficientnet-b3' --pretrained=False --train_test_split=0.25 --learning_rate=1e-2 --weight_decay=0
+#python3 transfer_learning.py --model='google/efficientnet-b3' --train_test_split=0.25 --learning_rate=1e-3 --weight_decay=0 --batch_size=16 --pretrained
 python3 transfer_learning.py --model='google/efficientnet-b3' --train_test_split=0.25 --learning_rate=1e-3 --weight_decay=0 --batch_size=16
 
