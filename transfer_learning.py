16adaac7e26c4b67e81516346d1b99676b1d6334
Jonah Krop <jonah.krop@EVITECORP-M-135.localdomain>
2024-11-27 09:24:16 -0800

    reverting save best mechanism

--- transfer_learning.py
+++ transfer_learning.py
@@ -118,9 +118,9 @@ class classifier():
             'eval_steps': 100,
             'logging_steps': 100,
             'per_device_eval_batch_size': self.per_device_train_batch_size,
-            'seed': 0,
-            'save_total_limit': 5,
-            'load_best_model_at_end': True
+            'seed': 0
+            # 'save_total_limit': 5,
+            # 'load_best_model_at_end': True
         }
         m_args = TrainingArguments(**args_d)
 
