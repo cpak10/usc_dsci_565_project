784a7a99fabf47091c6a48ab8f7bccef084a34b3
Kevin O'Kane <156379066+krokane@users.noreply.github.com>
2024-12-02 22:13:03 -0800

    facebook/convnext-base-224

--- transfer_learning.py
+++ transfer_learning.py
@@ -200,7 +200,7 @@ if __name__ == '__main__':
     print(args)
 
     valid_models = ['google/efficientnet-b3', 'google/efficientnet-b7', 'google/mobilenet_v2_1.0_224',
-                    'microsoft/resnet-18', 'facebook/deit-tiny-patch16-224', 'timm/resnext50_32x4d.a1_in1k']
+                    'microsoft/resnet-18', 'facebook/deit-tiny-patch16-224', 'facebook/convnext-base-224']
     if args.model not in valid_models:
         raise ValueError(f'for now, please use a model in {valid_models}')
 
